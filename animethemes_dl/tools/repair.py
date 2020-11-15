da8f226daa2f153a9782f5490bb31d7e3d660bfc
sadru <thesadru@gmail.com>
2020-11-15 10:35:23 +0100

    Fixed repair

--- animethemes_dl/tools/repair.py
+++ animethemes_dl/tools/repair.py
@@ -2,9 +2,8 @@
 Does general repair for animethemes-dl. This is mostly for updating
 """
 import logging
-from logging import log
 from os import PathLike, listdir, remove
-from os.path import isfile,join
+from os.path import isfile,join,realpath
 from typing import Set
 
 from ..models import DownloadData
@@ -13,19 +12,27 @@ from .audio import add_id3_metadata
 
 logger = logging.getLogger('animethemes-dl')
 
-def update_metadata(data: DownloadData):
+def update_metadata(data: DownloadData, need_permission: bool=True) -> int:
     """
-    Updates metadata for all mp3's
+    Updates metadata for all mp3's.
+    Returns amount of tagged files.
     """
+    totag = []
     for anime in data:
         path = anime['audio_path']
         if path is not None and isfile(path):
-            logger.info(f'[tag] readding metadata for {path}')
-            add_id3_metadata(
-                path,
-                anime['metadata'],
-                OPTIONS['download']['add_coverart']
-            )
+            totag.append((path,anime['metadata']))
+    
+    if need_permission:
+        answer = input(f'Tag {len(totag)} audio files? [y/n] ')
+        if answer.lower() != 'y':
+            return 0
+    
+    for path,metadata in totag:
+        logger.info(f'[tag] readding metadata for {path}')
+        add_id3_metadata(path,metadata,OPTIONS['download']['add_coverart'])
+    
+    return len(totag)
 
 def delete_unwanted(wanted: Set[PathLike], folder: PathLike, need_permission: bool=True) -> int:
     """
@@ -34,27 +41,22 @@ def delete_unwanted(wanted: Set[PathLike], folder: PathLike, need_permission: bo
     """
     todelete = []
     for path in listdir(folder):
-        path = join(folder,path)
+        path = realpath(join(folder,path))
         if path not in wanted:
             todelete.append(path)
     
     if need_permission:
-        if not todelete:
+        answer = input(f'Delete {len(todelete)} files from {folder}? [y/n] ')
+        if answer.lower() != 'y':
             return 0
-        if not OPTIONS['ignore_prompts']:
-            answer = input(f'Delete {len(todelete)} files from {folder}? [y/n] ')
-            
-            if answer.lower() != 'y':
-                return 0
-        
     
     for path in todelete:
-        remove(path)
         logger.info(f'[delete] Deleting {path}')
+        remove(path)
     
     return len(todelete)
 
-def delete_all_unwanted(data: DownloadData, video: bool=True, audio:bool=True):
+def delete_all_unwanted(data: DownloadData, video: bool=True, audio: bool=True, need_permission: bool=True):
     """
     Deletes all unwanted files with DownloadData.
     Choose which type to delete.
@@ -68,16 +70,17 @@ def delete_all_unwanted(data: DownloadData, video: bool=True, audio:bool=True):
         audiopaths.add(i['audio_path'])
     
     total = 0
-    if video: total+=delete_unwanted(videopaths,OPTIONS['download']['video_folder'])
-    if audio: total+=delete_unwanted(audiopaths,OPTIONS['download']['audio_folder'])
-    logger.info(f'[delete] Deleted {total} total files.')
+    if video: total+=delete_unwanted(videopaths,OPTIONS['download']['video_folder'],need_permission)
+    if audio: total+=delete_unwanted(audiopaths,OPTIONS['download']['audio_folder'],need_permission)
+    return total
 
 def repair(data: DownloadData):
     """
     Deletes unwanted files and updates metadata
     """
-    delete_all_unwanted(data)
-    update_metadata(data)
+    deleted = delete_all_unwanted(data,not OPTIONS['ignore_prompts'])
+    tagged = update_metadata(data,not OPTIONS['ignore_prompts'])
+    logger.info(f'[repair] deleted {deleted} files and retagged {tagged} audio files.')
 
 if __name__ == "__main__":
     from ..parsers import get_download_data
