4314bebf731a09ba0251512537e63fea4a8d7f39
sadru <thesadru@gmail.com>
2021-01-27 18:36:19 +0100

    finally figured out new animelists
    an update after a long imt

--- animethemes_dl/models/animelist.py
+++ animethemes_dl/models/animelist.py
@@ -2,7 +2,7 @@
 Normalized AnimeList type.
 """
 from datetime import datetime
-from typing import Literal, TypedDict
+from typing import Literal, TypeVar, TypedDict
 
 
 class AnimeListDict(TypedDict):
@@ -12,3 +12,5 @@ class AnimeListDict(TypedDict):
     score: Literal[0,1,2,3,4,5,6,7,8,9,10]
     priority: Literal[0,1,2]
     start_date: datetime
+
+AnimeListSite = Literal["Official Website", "Twitter", "aniDB", "AniList", "Anime-Planet", "Anime News Network", "Kitsu", "MyAnimeList", "Wiki"]
--- animethemes_dl/parsers/animethemes.py
+++ animethemes_dl/parsers/animethemes.py
@@ -1,13 +1,26 @@
 """
 Parses the animethemes api, getting data by title.
-Uses ThreadPoolExecutor.map to get data faster on slower connections.
+
+- load data from cache
+  - animethemes,unavalible = load_cache()
+    - animethemes: list[AnimeThemeAnime]
+    - unavalible: dict[site_name:set[site_id]]
+  - animelist = get_old()
+    - sorted by _fetched_at
+- organize animethemes into dict by id
+- start fetching data
+  - every time a request is fetched, update animethemes with it
+  - if the request is invalid, add it to unavalible
+- save data to cache
+- return animethemes
 """
+from animethemes_dl.models.animelist import AnimeListSite
 import json
 import logging
 from os.path import isfile
 import time
 from concurrent.futures import ThreadPoolExecutor
-from typing import Dict, Iterable, List, Optional, Tuple
+from typing import Dict, Iterable, List, Optional, Set, Tuple
 
 from pySmartDL.utils import get_random_useragent
 from requests import Session
@@ -24,15 +37,20 @@ session = Session()
 session.headers = {
     "User-Agent":get_random_useragent()
 }
+session.requests_remaining = float('inf')
 CACHEFILE = get_tempfile_path('animethemes.json')
 
 logger = logging.getLogger('animethemes-dl')
+
 
 def api_search(title: str) -> Dict[str,List[AnimeThemeAnime]]:
     """
     Requests a search from the api.
     """
+    if session.requests_remaining < 5:
+        return None
     r = session.get(URL.format(title))
+    session.requests_remaining = min(session.requests_remaining, int(r.headers.get('X-RateLimit-Remaining',0)))
     if r.status_code == 200:
         return r.json()
     elif r.status_code == 429:
@@ -40,126 +58,114 @@ def api_search(title: str) -> Dict[str,List[AnimeThemeAnime]]:
     else:
         r.raise_for_status()
 
-def make_anime_request(title: str) -> List[AnimeThemeAnime]:
+def verify_anime(animes: List[AnimeThemeAnime], alid: int, alsite: AnimeListSite) -> Optional[AnimeThemeAnime]:
+    """
+    Takes in a list of anime and figures out the correct one.
+    Returns None if no anime is right
+    alid is the external id in list site and alsite is the name of the anime list site.
+    """
+    for anime in animes:
+        for resource in anime['resources']:
+            if resource['site']==alsite and resource['external_id']==alid:
+                return anime
+    return None
+
+def fetch_anime(title: str, alid: int, alsite: AnimeListSite) -> Optional[AnimeThemeAnime]:
     """
     Requests an anime search with a title.
-    Strips out unexpected characters.
+    alid is the external id in list site and alsite is the name of the anime list site.
+    Can modify the title and send a request up to 3 times, before finding the right data.
     """
     for func in (remove_bracket,simplify_title,add_honorific_dashes):
         title = func(title)
-        anime = api_search(title)['anime']
-        if anime:
-            return anime
+        data = api_search(title)
+        if data is None: return None
+        anime = verify_anime(data['anime'],alid,alsite)
+        if anime is not None: return anime
     
-    return []
-
-def get_malid(anime: AnimeThemeAnime) -> int:
-    """
-    Returns anime theme of resource.
-    """
-    for resource in anime['resources']:
-        if resource["site"] == "MyAnimeList":
-            return resource['external_id']
-
-def pick_best_anime(malid: int, title: str, animes: List[AnimeThemeAnime]) -> Optional[AnimeThemeAnime]:
-    """
-    Goes through animes returned by api and returns the correct one.
-    """
-    for anime in animes:
-        if malid == get_malid(anime):
-            return anime
     return None
 
-def request_anime(animentry: Tuple[int,str]) -> Tuple[Tuple[int,str],Optional[AnimeThemeAnime]]:
+def load_cache(animelist: List[Tuple[str,int,AnimeListSite]]) -> (
+        Tuple[List[Tuple[str,int,AnimeListSite]], Dict[int,AnimeThemeAnime], Dict[Tuple[AnimeListSite,int],Tuple[str,int]]]):
     """
-    Makes a requests to the api with an anime entry.
-    Returns anime,themes
+    Loads cache and figures out which entries need to be updated.
     """
-    malid,title = animentry
-    animes = make_anime_request(title)
-    anime = pick_best_anime(malid,title,animes)
-    return animentry,anime
-
-def run_executor(animelist: List[Tuple[int,str]], progressbar: str='') -> Iterable[AnimeThemeAnime]:
-    """
-    Goes thorugh anime entries and yields their api returns.
-    """
-    measure = Measure()
-    total_fetched = 0
-    total_failed = 0
-    total_expected = len(animelist)
-    
-    with ThreadPoolExecutor(MAXWORKERS) as executor:
-        try:
-            for animentry,anime in executor.map(request_anime,animelist):
-                if anime:
-                    total_fetched += 1
-                else:
-                    total_failed += 1
-                    anime = {
-                        'id':None, # invalid id means that there's no entry in the API
-                        'title':animentry[1],
-                        'resources':[{'site':'MyAnimeList','external_id':animentry[0]}]
-                    } 
-                
-                anime['_fetched_at'] = time.time()
-                yield anime
-                
-                if progressbar:
-                    print(progressbar%(total_fetched+total_failed,total_expected),end='\r')
-                
-        except AnimeThemesTimeout as e:
-            logger.error(e.args[0])
-        except Exception as e:
-            logger.exception(e)
-    
-    if progressbar: logger.info(
-        f'[get] Got {total_fetched}/{total_expected}' + 
-        f'{f" ({total_failed} unavalible) " if total_failed else " "}' + 
-        f'entries from animethemes in {measure()}s.')
-
-def pick_needed(animelist: List[Tuple[int,str]]) -> Tuple[List[Tuple[int,str]],List[AnimeThemeAnime]]:
-    """
-    Takes in an animelist and returns a tuple of wanted animelist and a list of animethemes.
-    """
+    current_time = time.time()
     logger.debug(f'Loading animethemes data from {CACHEFILE}')
-    animethemes = []
-    animelist = dict(animelist)
+    animelist = {(site,alid):title for title,alid,site in animelist}
     
     with open(CACHEFILE) as file:
-        animethemes = sorted(json.load(file),key=lambda x:x['_fetched_at'])
-        for anime in animethemes: # loads sorted data
-            malid = get_malid(anime)
-            if malid in animelist and time.time()-anime['_fetched_at'] <= OPTIONS['download']['max_cache_age']:
-                del animelist[malid]
-    
-    return list(animelist.items()),animethemes        
+        data = json.load(file)
+        animethemes: list[AnimeThemeAnime] = sorted(data['anime'], key=lambda x:x['_fetched_at'])
+        unavalible: dict[tuple[AnimeListSite,int],tuple[str,int]] = {(i['site'],i['alid']):(i['title'],i['fetched_at']) for i in data['unavalible_anime']}
 
-def fetch_animethemes(animelist: List[Tuple[int,str]]) -> List[AnimeThemeAnime]:
-    """
-    Gets the anime entries from animethemes or a data file younger than a day.
-    Can show progress with `show_progress` string. Formats with % current,total.
-    """
-    progressbar = "[^] %s/%s" if logger.level<=logging.INFO else ""
-    if isfile(CACHEFILE):
-        animelist,animethemes = pick_needed(animelist)
-        if animelist:
-            animethemes = {get_malid(x):x for x in animethemes} # gets only unique ids
-            for anime in run_executor(animelist,progressbar):
-                animethemes[get_malid(anime)] = anime
-            animethemes = list(animethemes.values())
-    else:
-        animethemes = list(run_executor(animelist,progressbar))
+    for anime in animethemes:
+        for resource in anime['resources']:
+            if (resource['site'],resource['external_id']) in animelist and current_time-anime['_fetched_at'] <= OPTIONS['download']['max_cache_age']:
+                del animelist[resource['site'],resource['external_id']]
     
+    for (site,alid),(title,fetched_at) in unavalible.items():
+        if (site,alid) in animelist and current_time-fetched_at <= OPTIONS['download']['max_cache_age']:
+            del animelist[site,alid]
+    
+    animelist = [(title,alid,site) for (site,alid),title in animelist.items()]
+    animethemes = {anime['id']:anime for anime in animethemes}
+    
+    return animelist,animethemes,unavalible
+
+def save_cache(animethemes: List[AnimeThemeAnime], unavalible: Dict[Tuple[AnimeListSite,int],Tuple[str,int]]):
+    """
+    Saves data to cache.
+    """
+    logger.debug('Saving animethemes data at {CACHEFILE}')
+    unavalible = [{'title':title,'alid':alid,'site':site,'fetched_at':fetched_at} for (site,alid),(title,fetched_at) in unavalible.items()]
     with open(CACHEFILE,'w') as file:
-        logger.debug(f'Storing animethemes data in {CACHEFILE}')
-        json.dump(animethemes,file)
-    
-    return [anime for anime in animethemes if anime['id'] is not None] # remove unexisting api entries
+        json.dump({
+            'anime':animethemes,
+            'unavalible_anime':unavalible
+        },file)
 
-if __name__ == "__main__":
-    from .animelist import MyAnimeList
-    logger.setLevel(logging.DEBUG)
-    animethemes = fetch_animethemes(MyAnimeList().get_titles('sadru'))
-    with open('test.json','w') as file:
-        json.dump(animethemes,file,indent=4)
+def fetch_animethemes(animelist: List[Tuple[str,int,AnimeListSite]], use_cache=True) -> List[AnimeThemeAnime]:
+    m = Measure()
+    
+    if use_cache and isfile(CACHEFILE):
+        animelist,animethemes,unavalible = load_cache(animelist)
+    else:
+        animethemes,unavalible = {},{}
+    fetched,failed,total = 0,0,len(animelist)
+    
+    if animelist:
+        with ThreadPoolExecutor(MAXWORKERS) as executor:
+            futures = [(executor.submit(fetch_anime, *al),al) for al in reversed(animelist)]
+            
+            try:
+                while futures:
+                    anime,al = futures.pop()
+                    anime = anime.result()
+                    if anime is not None:
+                        anime['_fetched_at'] = time.time()
+                        animethemes[anime['id']] = anime
+                        fetched += 1
+                    else:
+                        unavalible[al[2],al[1]] = (al[0],time.time())
+                        failed += 1
+                if logger.level <= logging.INFO:
+                    print(f"\r^ {fetched+failed}/{total}",end='',flush=True)
+            
+            except AnimeThemesTimeout as e:
+                logger.error(f'[error] {e.args[0]}')
+                print(futures)
+            finally:
+                for future in futures: future[0].cancel()
+    
+    animethemes = list(animethemes.values())
+    if use_cache and animelist:
+        save_cache(animethemes,unavalible)
+    
+    if animelist:
+        if logger.level <= logging.INFO: print()
+        logger.info(f"[get] Got {fetched+failed}/{total} entries{(f' ({failed} unavalible) ' if failed else ' ')}from animethemes.moe")
+    else:
+        logger.info(f'[get] Loaded {len(animethemes)} cached entries from animethemes.moe')
+    
+    return animethemes
